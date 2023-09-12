7f33c302e54c85ffa7199e5ead19b01ed50412ef
mofa <ngekemofa@gmail.com>
2023-09-12 10:29:46 +0100

    A new dev shell script added

---
+++ dev.sh
@@ -0,0 +1,6 @@
+echo -n "Please enter your name: "
+read name
+echo -n "Please enter your team"
+read team
+echo "OHH Nice, welcome to Etech Devops master class $name and you are welcome to $team"
+echo "End of script"
